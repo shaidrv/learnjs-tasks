df686224e93a2788acb0c1f2abbf10d84b333e07
Rita <shaidrv@gmail.com>
2023-03-03 23:50:06 +0300

    5 глава задач

--- chapter_5. Data types/5.4. Arrays/task_3.js
+++ chapter_5. Data types/5.4. Arrays/task_3.js
@@ -1,1 +1,28 @@
-function getMaxSubSum(arr) {}
+// 1
+function getMaxSubSum(arr) {
+  let maxSum = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    let sumFromI = 0
+    for (j = i; j < arr.length; j++) {
+      sumFromI += arr[j]
+      maxSum = Math.max(maxSum, sumFromI)
+    }
+  }
+
+  return maxSum
+}
+
+// 2
+function getMaxSubSum(arr) {
+  let maxSum = 0
+  let subSum = 0
+
+  for (let item of arr) {
+    subSum += item
+    maxSum = Math.max(maxSum, subSum)
+    if (subSum < 0) subSum = 0
+  }
+
+  return maxSum
+}
---
+++ chapter_5. Data types/5.5. Array methods/task_2.js
@@ -0,0 +1,3 @@
+function filterRange(arr, a, b) {
+  return arr.filter((item) => item >= 2 && item <= b)
+}
